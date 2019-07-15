72d7a4775ec7c67fb5e426c968ba6d226bfc9aec
Wolfie <wolfiewerewolf@gmail.com>
2019-07-15 14:32:36 -0300

    skeleton project.

---
+++ src/test/MyTestCase.hx
@@ -0,0 +1,14 @@
+package test;
+
+/** https://haxe.org/manual/std-unit-testing.html */
+
+/** TestCase Boilerplate */
+class MyTestCase extends haxe.unit.TestCase {
+    override public function setup() {
+
+    }
+
+    override public function tearDown() {
+
+    }
+}
---
+++ src/test/TestRunner.hx
@@ -0,0 +1,13 @@
+package test;
+
+class TestRunner {
+    var r = new haxe.unit.TestRunner();
+
+    public function new() {
+        /** Add Test Cases here.... */
+
+
+        /** Run the tests*/
+        //r.run();
+    }
+}
